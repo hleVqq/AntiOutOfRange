f2eb4dc8f917c62d7a533c4e70f589d007a21a6d
hleV <hlevqq@gmail.com>
2019-07-24 02:15:19 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -7,11 +7,9 @@ int main()
 	HMONITOR handle = MonitorFromWindow(GetActiveWindow(), MONITOR_DEFAULTTOPRIMARY);
 	LPPHYSICAL_MONITOR monitors = (LPPHYSICAL_MONITOR)malloc(sizeof(PHYSICAL_MONITOR));
 
-	HANDLE monitor = NULL;
-
 	if (monitors && GetPhysicalMonitorsFromHMONITOR(handle, 1, monitors))
 	{
-		monitor = monitors[0].hPhysicalMonitor;
+		HANDLE monitor = monitors[0].hPhysicalMonitor;
 
 		DWORD cc = 0, dc = 0;
 		GetVCPFeatureAndVCPFeatureReply(monitor, 0xCC, NULL, &cc, NULL); printf("0xCC = %d\n", cc);
